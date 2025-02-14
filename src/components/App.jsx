336fecbe8ce95f0b26b47e11a3ac62e8023a4971
Novoh .K. Sada <kelvinsada23@gmail.com>
2025-02-14 13:01:17 +0100

    ddkjdw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,262 +1,262 @@
 import React,{useEffect, useState} from 'react';
 import './App.css';
-import cloudImage from "./picture/cloud.png";
-import Logo from "./picture/logo.png";
-import FormInput from './Forms-input';
-import TicketHead from './TicketHead';
+// import cloudImage from "./picture/cloud.png";
+// import Logo from "./picture/logo.png";
+// import FormInput from './Forms-input';
+// import TicketHead from './TicketHead';
 
 
 function App() {
-  const [imageUrl,setImageUrl] = useState()
-  const [firstError,setFirstError] = useState()
-  const [errors,setErrors] =useState()
-  const [savedFormData,setSavedFormData] = useState(false)
-  const [formInputsValue,setFormInputsValue] = useState({
-    ticketno:1,
-    name:"",
-    email:"",
-    textarea:""
-  })
+//   const [imageUrl,setImageUrl] = useState()
+//   const [firstError,setFirstError] = useState()
+//   const [errors,setErrors] =useState()
+//   const [savedFormData,setSavedFormData] = useState(false)
+//   const [formInputsValue,setFormInputsValue] = useState({
+//     ticketno:1,
+//     name:"",
+//     email:"",
+//     textarea:""
+//   })
     
-  const [picketTicket,setPickedTicket] = useState()
-  const [ticketType,setTicketType] = useState()
+//   const [picketTicket,setPickedTicket] = useState()
+//   const [ticketType,setTicketType] = useState()
     
-  // const [pageNo, setPageNo] = useState(0);
+//   // const [pageNo, setPageNo] = useState(0);
 
-    function bookNewTicket(){
-      setImageUrl()
-      setSavedFormData(false)
-      setFormInputsValue({
-    ticketno:1,
-    name:"",
-    email:"",
-    textarea:""
-  })
-  setPickedTicket()
-  setTicketType()
+//     function bookNewTicket(){
+//       setImageUrl()
+//       setSavedFormData(false)
+//       setFormInputsValue({
+//     ticketno:1,
+//     name:"",
+//     email:"",
+//     textarea:""
+//   })
+//   setPickedTicket()
+//   setTicketType()
     
-  // setPageNo(0);
-    }
-    //Send ticket to local storge
-  useEffect(()=>{
-    const data = localStorage.getItem("MY_SAVED_FORM_INFO")
-    if (data !== "undefined"){
-      setFormInputsValue(JSON.parse(data))
-    }
+//   // setPageNo(0);
+//     }
+//     //Send ticket to local storge
+//   useEffect(()=>{
+//     const data = localStorage.getItem("MY_SAVED_FORM_INFO")
+//     if (data !== "undefined"){
+//       setFormInputsValue(JSON.parse(data))
+//     }
   
-  },[])
+//   },[])
   
-  useEffect(()=>{
-      localStorage.setItem("MY_SAVED_FORM_INFO",JSON.stringify(formInputsValue))
-    // eslint-disable-next-line
-  },[formInputsValue])
+//   useEffect(()=>{
+//       localStorage.setItem("MY_SAVED_FORM_INFO",JSON.stringify(formInputsValue))
+//     // eslint-disable-next-line
+//   },[formInputsValue])
 
   
   
-  async function handlePictureUpload(event){
-    const imageFile = event.target.files[0]
-    if (!imageFile)return
+//   async function handlePictureUpload(event){
+//     const imageFile = event.target.files[0]
+//     if (!imageFile)return
     
-    const data = new FormData()  //created a formData object
-    data.append("file",imageFile)  //(file) keyword name ill use in cloudinary
-    data.append("upload_preset","hngtask_user_uploaded_photo")  //the name i used in cloudinary site
-    data.append("cloud_name","dshceqjzm") //go to api key in cloudinary, youll see the name
+//     const data = new FormData()  //created a formData object
+//     data.append("file",imageFile)  //(file) keyword name ill use in cloudinary
+//     data.append("upload_preset","hngtask_user_uploaded_photo")  //the name i used in cloudinary site
+//     data.append("cloud_name","dshceqjzm") //go to api key in cloudinary, youll see the name
     
-    const response = await fetch("https://api.cloudinary.com/v1_1/dshceqjzm/image/upload",{
-      method:"POST",
-      body:data,
-    })
-    const uploadedImageUrl = await response.json()
-    setImageUrl(uploadedImageUrl.url)
-  }
-  function handleSubmit(event){
-    // console.log(event)
-  }
-
-
-  function DownloadImage(image){
-    setImageUrl(image)    
-  }
-
-    //Saving image to local storage
-  useEffect(()=>{
-    const dataImage = localStorage.getItem("MY_SAVED_IMAGE")
-    if (dataImage !== "undefined"){
-      
-      DownloadImage(JSON.parse(dataImage))
-    }
-  },[])
-
-  useEffect(()=>{
-      localStorage.setItem("MY_SAVED_IMAGE",JSON.stringify(imageUrl))
-    // eslint-disable-next-line
-  },[imageUrl])
-
-
-    //Saving TicketType to local storage
-    useEffect(()=>{
-      const ticket = localStorage.getItem("Ticket_type")
-      if (ticket !== "undefined"){
-        setTicketType(JSON.parse(ticket))
-      }
-    },[])
-
-    useEffect(()=>{
-      localStorage.setItem("Ticket_type",JSON.stringify(ticketType))
-    // eslint-disable-next-line
-  },[ticketType])
-
-
-//Saving TicketNo to local storage
-useEffect(()=>{
-  const ticketno = localStorage.getItem("Ticket_number")
-  if (ticketno !== "undefined"){
-    setPickedTicket(JSON.parse(ticketno))
-  }
-},[])
-
-
-useEffect(()=>{
-  localStorage.setItem("Ticket_number",JSON.stringify(picketTicket))
-// eslint-disable-next-line
-},[picketTicket])
-
-
-//Saving Page number to local storage
-// useEffect(()=>{
-//   const page = window.localStorage.getItem("My_Page_No")
-//   if (page !== undefined && page !== null && page !== "undefined" && page !== "null"){
-//    console.log("page has something")
-//     setPageNo(JSON.parse(page))
-
-//   }else{
-//     console.log("page does not have something")
-//     setPageNo(0)
+//     const response = await fetch("https://api.cloudinary.com/v1_1/dshceqjzm/image/upload",{
+//       method:"POST",
+//       body:data,
+//     })
+//     const uploadedImageUrl = await response.json()
+//     setImageUrl(uploadedImageUrl.url)
 //   }
-//   // setPageNo(JSON.parse(page))
-//   // console.log(JSON.parse(page))
-//   // if (page == "null"){
-//   //   alert("page is null")
-//   // } else{
-//   //   alert("page is not null")
-//   // }
-
-//   // if (page !== "undefined" ){
-//   //   setPageNo(JSON.parse(page))
-//   // } else{
-//   //   setPageNo(0)
-//   // }
-
-//   // if (page === "null" || page === "undefined"){
-//   //   setPageNo(0)
-//   // }
-// },[])
-// console.log(pageNo)
-// useEffect(()=>{
-//   window.localStorage.setItem("My_Page_No",JSON.stringify(pageNo))
-// // eslint-disable-next-line
-// },[pageNo])
+//   function handleSubmit(event){
+//     // console.log(event)
+//   }
 
 
-  function isValidEmail(email){
-    const emailRegex = /^\S+@\S+\.\S+$/;
-    return emailRegex.test(email)
-  }
-  
-function handleValue(e){
-  const value = e.target.value
-  const name = e.target.name
-  
-  setFormInputsValue(prev=>{
-    return{
-      ...prev,
-      [name]:value
-    }
-  })
-  setSavedFormData(!savedFormData)
-  
-}
-function firstAction(){
-  let error ={}
-  if (!formInputsValue.ticketno){
-    error.ticketno="Please select the no of tickets"
-  }
-  if (!ticketType){
-    error.selectedTicket="Choose type of tickets"
-  }
+//   function DownloadImage(image){
+//     setImageUrl(image)    
+//   }
 
-  setFirstError(error)
+//     //Saving image to local storage
+//   useEffect(()=>{
+//     const dataImage = localStorage.getItem("MY_SAVED_IMAGE")
+//     if (dataImage !== "undefined"){
       
-  // if (ticketType && formInputsValue.ticketno >0){
-  //     setPageNo(prev=>{
-  //        return prev+1
-  //        })
-  // }
-}
-function handleSubmitClick(event){
-  let newErrors = {}
+//       DownloadImage(JSON.parse(dataImage))
+//     }
+//   },[])
+
+//   useEffect(()=>{
+//       localStorage.setItem("MY_SAVED_IMAGE",JSON.stringify(imageUrl))
+//     // eslint-disable-next-line
+//   },[imageUrl])
+
+
+//     //Saving TicketType to local storage
+//     useEffect(()=>{
+//       const ticket = localStorage.getItem("Ticket_type")
+//       if (ticket !== "undefined"){
+//         setTicketType(JSON.parse(ticket))
+//       }
+//     },[])
+
+//     useEffect(()=>{
+//       localStorage.setItem("Ticket_type",JSON.stringify(ticketType))
+//     // eslint-disable-next-line
+//   },[ticketType])
+
+
+// //Saving TicketNo to local storage
+// useEffect(()=>{
+//   const ticketno = localStorage.getItem("Ticket_number")
+//   if (ticketno !== "undefined"){
+//     setPickedTicket(JSON.parse(ticketno))
+//   }
+// },[])
+
+
+// useEffect(()=>{
+//   localStorage.setItem("Ticket_number",JSON.stringify(picketTicket))
+// // eslint-disable-next-line
+// },[picketTicket])
+
+
+// //Saving Page number to local storage
+// // useEffect(()=>{
+// //   const page = window.localStorage.getItem("My_Page_No")
+// //   if (page !== undefined && page !== null && page !== "undefined" && page !== "null"){
+// //    console.log("page has something")
+// //     setPageNo(JSON.parse(page))
+
+// //   }else{
+// //     console.log("page does not have something")
+// //     setPageNo(0)
+// //   }
+// //   // setPageNo(JSON.parse(page))
+// //   // console.log(JSON.parse(page))
+// //   // if (page == "null"){
+// //   //   alert("page is null")
+// //   // } else{
+// //   //   alert("page is not null")
+// //   // }
+
+// //   // if (page !== "undefined" ){
+// //   //   setPageNo(JSON.parse(page))
+// //   // } else{
+// //   //   setPageNo(0)
+// //   // }
+
+// //   // if (page === "null" || page === "undefined"){
+// //   //   setPageNo(0)
+// //   // }
+// // },[])
+// // console.log(pageNo)
+// // useEffect(()=>{
+// //   window.localStorage.setItem("My_Page_No",JSON.stringify(pageNo))
+// // // eslint-disable-next-line
+// // },[pageNo])
+
+
+//   function isValidEmail(email){
+//     const emailRegex = /^\S+@\S+\.\S+$/;
+//     return emailRegex.test(email)
+//   }
   
-  if (!imageUrl){
-    newErrors.image = "Please put an Image"
-  }
-  if (!formInputsValue.ticketno){
-    newErrors.ticketno="Please select the no of tickets"
-  }
-  if (!formInputsValue.name){
-    newErrors.name="Full name is required"
-  }
-  if (!formInputsValue.email){
-    newErrors.email = "Email is required"
-  } else if (!isValidEmail(formInputsValue.email)){
-    newErrors.email ="Invalid email format"
-  }
-  if(!formInputsValue.textarea){
-    newErrors.textarea="Texts is required"
-  }
-  setErrors(newErrors)
+// function handleValue(e){
+//   const value = e.target.value
+//   const name = e.target.name
+  
+//   setFormInputsValue(prev=>{
+//     return{
+//       ...prev,
+//       [name]:value
+//     }
+//   })
+//   setSavedFormData(!savedFormData)
+  
+// }
+// function firstAction(){
+//   let error ={}
+//   if (!formInputsValue.ticketno){
+//     error.ticketno="Please select the no of tickets"
+//   }
+//   if (!ticketType){
+//     error.selectedTicket="Choose type of tickets"
+//   }
 
-  // if (imageUrl && formInputsValue.name && newErrors.email === undefined &&formInputsValue.textarea){
-  //   setPageNo(prev=>prev+1)
-  // }
-  event.preventDefault()
- }
+//   setFirstError(error)
+      
+//   // if (ticketType && formInputsValue.ticketno >0){
+//   //     setPageNo(prev=>{
+//   //        return prev+1
+//   //        })
+//   // }
+// }
+// function handleSubmitClick(event){
+//   let newErrors = {}
+  
+//   if (!imageUrl){
+//     newErrors.image = "Please put an Image"
+//   }
+//   if (!formInputsValue.ticketno){
+//     newErrors.ticketno="Please select the no of tickets"
+//   }
+//   if (!formInputsValue.name){
+//     newErrors.name="Full name is required"
+//   }
+//   if (!formInputsValue.email){
+//     newErrors.email = "Email is required"
+//   } else if (!isValidEmail(formInputsValue.email)){
+//     newErrors.email ="Invalid email format"
+//   }
+//   if(!formInputsValue.textarea){
+//     newErrors.textarea="Texts is required"
+//   }
+//   setErrors(newErrors)
+
+//   // if (imageUrl && formInputsValue.name && newErrors.email === undefined &&formInputsValue.textarea){
+//   //   setPageNo(prev=>prev+1)
+//   // }
+//   event.preventDefault()
+//  }
             
-function backButton(event){
-  // if (pageNo !== 0){
-  //   setPageNo(prev=>{
-  //     return prev-1
-  //   })
-  // }
-  event.preventDefault()
-}
+// function backButton(event){
+//   // if (pageNo !== 0){
+//   //   setPageNo(prev=>{
+//   //     return prev-1
+//   //   })
+//   // }
+//   event.preventDefault()
+// }
 
 
-const ticketsArray = [{
-  id:0,
-  type:"FREE",
-  name:"REGULAR ACCESS",
-  number:"20/52"
-},{
-  id:1,
-  type:"$150",
-  name:"VIP ACCESS",
-  number:"20/52"
-},{
-  id:2,
-  type:"$150",
-  name:"VVIP ACCESS",
-  number:"20/52"
-}]
+// const ticketsArray = [{
+//   id:0,
+//   type:"FREE",
+//   name:"REGULAR ACCESS",
+//   number:"20/52"
+// },{
+//   id:1,
+//   type:"$150",
+//   name:"VIP ACCESS",
+//   number:"20/52"
+// },{
+//   id:2,
+//   type:"$150",
+//   name:"VVIP ACCESS",
+//   number:"20/52"
+// }]
 
-function handleTicketPick(value){
-  // console.log(value.id)
-setTicketType(value.name)
-setPickedTicket(value.id)
-}
+// function handleTicketPick(value){
+//   // console.log(value.id)
+// setTicketType(value.name)
+// setPickedTicket(value.id)
+// }
   return (
     <div className="App">
       <header className="header">
-      <img className="logo-image" src={Logo} alt=""/>
+      {/* <img className="logo-image" src={Logo} alt=""/> */}
       <nav className="nav-menu">
         <ul className="menu-container">
           <li className="menu-Items">Event</li>
@@ -286,7 +286,7 @@ setPickedTicket(value.id)
       <div className='bottom-section1'>
         <p className='select-ticket-text'>Select Ticket type</p>
         <div className='ticket-type-container'>
-          { 
+          {/* { 
           ticketsArray.map((ticket,index)=>(
           <div onClick={()=>handleTicketPick(ticket)} key={ticket.id} className={(index)===picketTicket?'picked-ticket card-component':"card-component"}>
             <h1 className="card-component-head">{ticket.type}</h1>
@@ -294,21 +294,21 @@ setPickedTicket(value.id)
             <p className="card-component-lower-bottom">{ticket.number}</p>
           </div>
         ))
-      }
+      } */}
         </div>
-          {firstError&&<div className='error'>{firstError.selectedTicket}</div>}
-        <FormInput 
+          {/* {firstError&&<div className='error'>{firstError.selectedTicket}</div>} */}
+        {/* <FormInput 
           label={"Number of tickets"}
           type={"number"}
           value={formInputsValue.ticketno}
           name={"ticketno"}
           action={handleValue}
-        />
-          {firstError&&<div className='error'>{firstError.ticketno}</div>}
-          <div className='submit-buttons'>
+        /> */}
+          {/* {firstError&&<div className='error'>{firstError.ticketno}</div>} */}
+          {/* <div className='submit-buttons'>
             <button onClick={backButton} className='first-button'>Cancel</button>
             <button type="submit" onClick={firstAction} className='second-button'>Next</button>
-          </div>
+          </div> */}
       </div>
     </div>
 
@@ -328,19 +328,19 @@ setPickedTicket(value.id)
           <div className="upload-photo-container">
               
               <div className='custom-image'>
-              <label style={{backgroundImage:`url("${imageUrl}")`,backgroundSize:"contain",}} className="custom-image" htmlFor="myImage">
+              {/* <label style={{backgroundImage:`url("${imageUrl}")`,backgroundSize:"contain",}} className="custom-image" htmlFor="myImage">
                 <img src={cloudImage} alt=""/>
                 <p className='upload-image-text'>Drag and drop or click to upload</p>
-              </label>
+              </label> */}
               </div>
               
-              <input id="myImage" className='inner-image-container' type="file" onChange={handlePictureUpload}/>
+              {/* <input id="myImage" className='inner-image-container' type="file" onChange={handlePictureUpload}/> */}
 
           </div>
-              {errors&&<div className='error'>{errors.image}</div>}
+              {/* {errors&&<div className='error'>{errors.image}</div>} */}
         </div>
 
-        <form onSubmit={handleSubmit} className="form-container">
+        {/* <form onSubmit={handleSubmit} className="form-container">
           <FormInput 
           label={"Enter your Fullname"}
           type={"text"}
@@ -368,7 +368,7 @@ setPickedTicket(value.id)
             <button onClick={backButton} className='first-button'>Back</button>
             <button type="submit" className='second-button' onClick={handleSubmitClick}>Get My Free Ticket</button>
           </div>
-        </form>
+        </form> */}
       </div>
     </div>
     {/* } */}
@@ -393,10 +393,10 @@ setPickedTicket(value.id)
             <h1 className='ticket-content-head'>Techember Fest "25</h1>
             <p className='ticket-content-text-first'>📍 04 Rumens road, Ikoyi, Lagos</p>
             <p className='ticket-content-text-first'>📅 March 15, 2025 | 7:00 PM</p>
-            <img className="ticket-content-pic" src={imageUrl} alt=""/> 
+            {/* <img className="ticket-content-pic" src={imageUrl} alt=""/>  */}
             
           <div className='ticket-content-bottom'>
-            <table>
+            {/* <table>
               <tr >
                 <th className='first-table'>Enter your name</th>
                 <th>Enter your email*</th>
@@ -421,14 +421,14 @@ setPickedTicket(value.id)
               <td>{formInputsValue.textarea}</td>
               </td>
             </tr>
-          </table>
+          </table> */}
           </div>
-            <img className="barcode-image" src="https://i.ibb.co/W3cK42J/image-1.png" alt=""/>       
+            {/* <img className="barcode-image" src="https://i.ibb.co/W3cK42J/image-1.png" alt=""/>        */}
           </div>
         </div> 
       </div>
       <div className='submit-buttons'>
-            <button onClick={bookNewTicket} className='first-button'>Book another ticket</button>
+            {/* <button onClick={bookNewTicket} className='first-button'>Book another ticket</button> */}
             <button className='second-button' type="submit">Download Ticket</button>
       </div>
       </div>
